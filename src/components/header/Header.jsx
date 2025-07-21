f10c58ba2adbd2f0959aca365c7fe5ba81176f10
Rahul Dwivedi <aaryarahul205@gmail.com>
2025-07-21 16:57:46 +0530

    refactor: update styles and improve header component functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = () => {
-    if (searchQuery.trim()) {
+    if (searchQuery) {
       window.location.href = `/search/${searchQuery}`;
     }
   };
@@ -15,20 +15,13 @@ const Header = () => {
     <header className="header">
       <div className="headerLeft">
         <Link to="/">
-          <img className="header__icon" src="imdb.png" alt="IMDb Logo" />
+          <img src="imdb.png" alt="IMDb Logo" className="header__icon" />
         </Link>
-        <Link to="/movies/popular" className="header__link">
-          <span>Popular</span>
-        </Link>
-        <Link to="/movies/top_rated" className="header__link">
-          <span>Top Rated</span>
-        </Link>
-        <Link to="/movies/upcoming" className="header__link">
-          <span>Upcoming</span>
-        </Link>
+        <Link to="/movies/popular" className="header__link">Popular</Link>
+        <Link to="/movies/top_rated" className="header__link">Top Rated</Link>
+        <Link to="/movies/upcoming" className="header__link">Upcoming</Link>
       </div>
 
-      {}
       <div className="headerRight">
         <input
           type="text"
@@ -37,9 +30,7 @@ const Header = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
           className="searchInput"
         />
-        <button onClick={handleSearch} className="searchButton">
-          Search
-        </button>
+        <button onClick={handleSearch} className="searchButton">Search</button>
       </div>
     </header>
   );
